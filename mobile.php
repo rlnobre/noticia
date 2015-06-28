c173048673931cc7589b37c3d5314036ef7c80b2
rlnobre <rlnobre@gmail.com>
2015-06-28 16:51:39 -0300

    alteração de estilo mobile
    Alteração do CSS para mudar estilo da página Mobile.

--- mobile.php
+++ mobile.php
@@ -8,16 +8,17 @@ $desconecta;
 <!DOCTYPE html> 
 <html> 
 	<head> 
-	<title>Noticia Mobile</title> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css" />
+	<title>Not&iacutecias Mobile</title> 
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css" />	
+	<link rel="STYLESHEET" type="text/css" href="estilos.css">
 	<script type="text/javascript" src="http://code.jquery.com/jquery-1.5.min.js"></script>
 	<script type="text/javascript" src="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.js"></script>
 </head> 
 <body> 
 
-<div data-role="page">
-	<div data-role="header">
-		<h1>Noticias</h1>
+<div class="box_mobile" data-role="page">
+	<div class="tbox" data-role="header">
+		<p class="p_mobile">Not&iacutecias </p>
 	</div>
 
 	<div data-role="content">	
